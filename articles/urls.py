6df73a02574b24d81c462efbcc6509196ce37c23
shahriyorqosimov54@gmail.com <129074287+shakhriyor080@users.noreply.github.com>
2023-12-21 14:31:26 +0500

    meseg

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView, ArticleDeleteView
 
 urlpatterns = [
      path('', ArticleListView.as_view(),name='article_list'),
-        
+     path('<int:pk>/', ArticleDetailView.as_view() ,name='article_detail'),
+     path('<int:pk>/delete/', ArticleDeleteView.as_view() ,name='article_delete'),
+
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Article
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model  = Article
+    template_name  = 'article_detail.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
