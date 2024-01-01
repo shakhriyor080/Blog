46efb4f1a959b07abc436df8f7fc63cb67df5f8d
shahriyorqosimov54@gmail.com <129074287+shakhriyor080@users.noreply.github.com>
2024-01-02 00:27:48 +0500

    search tahlandi

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView, ArticleDeleteView
+from .views import ArticleListView, ArticleDetailView, ArticleDeleteView,ArticleUpdateView,ArticleCreateVeiw,Search
 
 urlpatterns = [
      path('', ArticleListView.as_view(),name='article_list'),
      path('<int:pk>/', ArticleDetailView.as_view() ,name='article_detail'),
      path('<int:pk>/delete/', ArticleDeleteView.as_view() ,name='article_delete'),
+     path('<int:pk>/edit/', ArticleUpdateView.as_view() ,name='article_edit'),
+     path('new/', ArticleCreateVeiw.as_view() ,name='article_new'),
+     path('search/', Search.as_view(), name='search'),
 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Article
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
@@ -12,7 +14,49 @@ class ArticleDetailView(DetailView):
     template_name  = 'article_detail.html'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user 
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+    model = Article
+    fields = ('title', 'summary', 'body', 'photo',)
+    template_name = 'article_edit.html'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user 
+
+class ArticleCreateVeiw(UserPassesTestMixin, LoginRequiredMixin,CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ('title','summary','body','photo','author',)
+
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+
+class Search(ListView):
+    template_name = 'article_list.html'
+    context_object_name = 'news'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return  Article.objects.filter(title__icontains=self.request.GET.get('q'))
+    
+    def get_context_data(self, * , object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
+
